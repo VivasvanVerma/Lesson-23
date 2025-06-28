9512cd715d844f3d0a077c81a0f85839661acd7f
VivasvanVerma <vvofficial.yt@gmail.com>
2025-06-28 15:40:53 +0530

    ccode

---
+++ ccode.py
@@ -0,0 +1,6 @@
+country_code = {'India':'0091', 'Australia':'0025', 'Nepal':'0977'}
+print("Country code for India: ")
+print(country_code.get('India', 'Not found'))
+
+print("Country code for Nepal: ")
+print(country_code.get('Nepal', 'Not found'))
